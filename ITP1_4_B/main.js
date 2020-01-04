faa61951fc410ed572b338211499e47b30a1a9e5
Y.Otsuki <vloxolv.otky.vlowolv@gmail.com>
2020-01-04 23:32:56 +0900

    ITP1_4_B

--- ITP1_4_B/main.js
+++ ITP1_4_B/main.js
@@ -1,6 +1,6 @@
 function Main(input) {
-  const [a, b] = input.split(' ').map(Number);
-  console.log(`${Math.floor(a / b)} ${a % b} ${(a / b).toFixed(5)}`);
+  const r = Number(input);
+  console.log(`${(Math.PI * r * r).toFixed(6)} ${(2 * Math.PI * r).toFixed(6)} `);
 }
 
 Main(require("fs").readFileSync("/dev/stdin", "utf8"));
